0c73125a0ca73dc8a6118488e0e0581ed317ab19
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-03-20 18:51:51 +0300

    update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-USE hbnb_test;
 GRANT ALL PRIVILEGES 
 ON `hbnb_test_db`.*
 TO 'hbnb_test'@'localhost';
