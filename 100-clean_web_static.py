e4bcd52b25c458d730e025b9bdca64ea1dd09e3b
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-04-04 19:36:25 -0700

    tasks 3 - 5

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -64,3 +64,19 @@ def deploy():
     except Exception as err:
         return False
 
+@task
+def do_clean(number=0):
+    """clean up old versions"""
+    try:
+        number = int(number)
+        if number < 0:
+            return False
+        if number == 0 or number == 1:
+            number = 1
+        local(f'cd versions; ls -t | tail -n +{number} | xargs rm -rf --')
+        for host in env.hosts:
+            env.host_string = host
+            run(f'cd /data/web_static/releases; ls -t | tail -n +{number} | xargs rm -rf --')
+        return True
+    except Exception as err:
+        return False
