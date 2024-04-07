50cace08756e73a816b3b620f5ab1af7dca91b50
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-04-07 13:40:38 -0700

    create 0-setup_web_static.sh file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
+# Prepare your web servers
+
+# for install nginx
 sudo apt-get update -y
 sudo apt-get install nginx -y
+
+# for create folders
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo sh -c 'echo "this is Emad"> /data/web_static/releases/test/index.html'
+sudo sh -c 'echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"> /data/web_static/releases/test/index.html'
+# for create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
+
+# for update the Nginx conf to serve the content
 sudo sh -c 'echo "server {
     listen 80;
     listen [::]:80 default_server;
-    server_name 52.3.243.117;
     location /hbnb_static {
         alias /data/web_static/current/;
     }
 }" > /etc/nginx/sites-available/default'
+
+# for restart nginx
 sudo service nginx restart
