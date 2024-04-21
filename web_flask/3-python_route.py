377c1ecd6ee3568ce84d26817fa2e00d439a3f73
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-04-21 05:25:57 -0700

    cre3-python_route.pyate  file

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Route /"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route /hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """display “Python ”, followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    """web application that listening on 0.0.0.0, port 5000"""
+    app.run(host='0.0.0.0', port=5000)
