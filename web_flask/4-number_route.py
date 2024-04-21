ad7b70f93cb1f69a49ffb21095ebe6d671a9e6a5
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-04-21 06:19:42 -0700

    create 4-number_route.py file

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,49 +1,6 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
 from flask import Flask
-from markupsafe import escape
-
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def hello_route():
-    """Route /"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Route /hbnb"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
-    """followed by the value of the text variable"""
-    return "C {}".format(text.replace("_", " "))
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """display “Python ”, followed by the value of the text variable"""
-    return "Python {}".format(text.replace("_", " "))
-
-
-@app.route("/number", strict_slashes=False)
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """Route Number."""
-    return f"{escape(n)} is a number"
-
-
-if __name__ == '__main__':
-    """web application that listening on 0.0.0.0, port 5000"""
-    app.run(host='0.0.0.0', port=5000)#!/usr/bin/python3
-"""a script that starts a Flask web application"""
-from flask import Flask
-from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -77,7 +34,7 @@ def python(text="is cool"):
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """display “n is a number” only if n is an integer"""
-    return f"{escape(n)} is a number"
+    return "{} is a number".format(n)
 
 
 if __name__ == '__main__':
