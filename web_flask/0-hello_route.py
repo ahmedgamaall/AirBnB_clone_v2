12c94bea77bacdceed09946cba3db6139bb2bdb1
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-05-16 05:10:39 -0700

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/birbnb-onepage/", strict_slashes=False)
 def hello_route():
     """Route /"""
     return "Hello HBNB!"
